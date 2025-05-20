dd8bb035d0d6191c45a4bb461ffa2249ba35d3a7
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-20 09:29:13 -0300

    Aula 1 e 2 Concluido

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP E Orientação A Objetos</title>
+</head>
+<body>
+    <!-- _____________AULA 1 INTRODUÇÃO A ORIENTAÇÃO A OBJETOS - CONSIDERAÇÕES PRELIMIARES_____________ -->
+    <!-- Concluido -->
+    <!-- _____________AULA 2 INTRODUÇÃO A ORIENTAÇÃO A OBJETOS_____________ -->
+    <!-- Concluido -->
+    <!-- _____________AULA 3 OO - PILAR DA ABSTRAÇÃO_____________ -->
+    
+    <!-- _____________AULA 4 GETTERS E SETTERS____________ -->
+    
+    <!-- _____________AULA 5 GETTERS E SETTERS MÁGICOS (OVERLOADING DE ATRIBUTOS E MÉTODOS)____________ -->
+    
+    <!-- _____________AULA 6 CHAMANDO MÉTODOS INTERNAMENMTE____________ -->
+    
+    <!-- _____________AULA 7 MÉTODOS CONSTRUTOR E DESTRUTOR (CONSTRUCT E DESTRUCT)____________ -->
+    
+    <!-- _____________AULA 8 OO - PILAR DA HERANÇA____________ -->
+    
+    <!-- _____________AULA 9 OO - PILAR DO POLIMORFISMO____________ -->
+    
+    <!-- _____________AULA 10 OO - PILAR DO EMCAPSULAMENTO PARTE 1____________ -->
+    
+    <!-- _____________AULA 11 OO - PILAR DO EMCAPSULAMENTO PARTE 2____________ -->
+    
+    <!-- _____________AULA 12 ATRIBUTOS E MÉTODOS ESTÁTICOS____________ -->
+    
+    <!-- _____________AULA 12 + 1 INTERFACES____________ -->
+    
+    <!-- _____________AULA 14 NAMESPACES PARTE 1 - UTILIZANDO NAMESPACES PARA CLASSES E INTERFACES____________ -->
+    
+    <!-- _____________AULA 15 NAMESPACES PARTE 2 - IMPORTANDO E APELIDANDO NAMESPACES (USE E ALIASING)____________ -->
+    
+    <!-- _____________AULA 16 TRATAMENTO DE ERROS - TRY, CATCH, FINALLY E THROW____________ -->
+    
+    <!-- _____________AULA 17 TRATAMENTO DE ERROS - EXCEÇÕES CUSTOMIZADAS____________ -->
+    
+    <!-- _____________AULA 18 APP SEND MAIL - INTRODUÇÃO____________ -->
+    
+    <!-- _____________AULA 19 APP SEND MAIL - INICIANDO PROJETO____________ -->
+    
+    <!-- _____________AULA 20 APP SEND MAIL - ENVIANDO DADOS DO FRONT-END PARA O BACK-END VIA MÉTODO POST____________ -->
+    
+    <!-- _____________AULA 21 APP SEND MAIL - CRAINDO E INSTANCIANDO A CLASSE MENSAGEM____________ -->
+    
+    <!-- _____________AULA 22 APP SEND MAIL - ADICIONANDO A BIBLIOTECA PHPMAILER AO PROJETO____________ -->
+    
+    <!-- _____________AULA 23 APP SEND MAIL - CONFIGURANDO O PHPMAILER E ENVIANDO E-MAILS____________ -->
+    
+    <!-- _____________AULA 23 + 1 IMPORTANTE - AJUSTANDO AS CONFIGURAÇÕES DE ACESSO AO SMTP DO GMAIL____________ -->
+    
+    <!-- _____________AULA 25 APP SEND MAIL - ENVIANDO EMAILS COM BASE NOS PARÂMETROS DO FRONT-END____________ -->
+    
+    <!-- _____________AULA 26 APP SEND MAIL - MELHORANDO O FEEDBACK VISUAL____________ -->
+    
+    <!-- _____________AULA 27 APP SEND MAIL - DE OLHO NA SEGURANÇA DO BACK-END____________ -->
+    
+</body>
+</html>
